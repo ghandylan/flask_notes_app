7d2b05e42ce17566b5788f09a56da8d0a32a090a
ghandylan <dylanlouis.tayag@yahoo.com>
2022-05-15 21:58:23 +0800

    Tweaked navbar and footer for base.html

--- auth.py
+++ auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('/auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login<p>"
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout<p>"
+    return render_template('logout.html')
 
 @auth.route('/register')
 def register():
-    return "<p>Register<p>"
+    return render_template('register.html')
