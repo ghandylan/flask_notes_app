b61ec1c0ceffd5690db1faf9f29f562bd4fdcd0b
ghandylan <dylanlouis.tayag@yahoo.com>
2022-05-17 03:28:02 +0800

    commited

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 my_database = SQLAlchemy()
 DATABASE_NAME = "database.my_database"
@@ -10,6 +11,7 @@ def createApp():
     app = Flask(__name__, template_folder='templates')
     app.config['SECRET_KEY'] = 'mysecret'
     app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{DATABASE_USER}:{DATABASE_PASSWORD}@localhost/{DATABASE_NAME}"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     my_database.init_app(app)
 
     from views import views
@@ -18,4 +20,10 @@ def createApp():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from db_models import Note, User
     return app
+
+def create_db(app):
+    if not path.exists('flask_notes_crud/' + DATABASE_NAME):
+        my_database.create_all(app=app)
+        print("Database created")
--- db_models.py
+++ db_models.py
@@ -1,12 +1,12 @@
 from enum import unique
 from time import timezone
-from . import my_database
+from __init__ import my_database
 from flask_login import UserMixin
 from sqlalchemy import func
 from sqlalchemy.dialects.mysql import *
 
 class Note(my_database.Model):
-    id = my_database.Column(my_database.Integer, primary_key=True)
+    id = my_database.Column(my_database.INT, primary_key=True)
     data = my_database.Column(my_database.String(9999))
     date = my_database.Column(my_database.DateTime(timezone=True), default=func.now())
     user_id = my_database.Column(my_database.Integer, my_database.ForeignKey('user.id'))
@@ -14,18 +14,16 @@ class Note(my_database.Model):
 
 class User(UserMixin, my_database.Model):
     id = my_database.Column(my_database.INT, primary_key=True)
-    name = my_database.Column(my_database.VARCHAR(255))
+    first_name = my_database.Column(my_database.VARCHAR(255))
+    last_name = my_database.Column(my_database.VARCHAR(255))
     email = my_database.Column(my_database.VARCHAR(255), unique=True)
-    phone = my_database.Column(my_database.VARCHAR(255))
+    phone = my_database.Column(my_database.VARCHAR(255), unique=True)
     password = my_database.Column(my_database.VARCHAR(255))
-
-
+    notes = my_database.relationship('Note')
     def __init__(self, name, email, phone, password):
         self.name = name
         self.email = email
         self.phone = phone
         self.password = password
-
-
     def __repr__(self):
         return f"User('{self.name}', '{self.email}', '{self.phone}', '{self.password}')"
