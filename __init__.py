826d7c99391810697db0c9e797279e5eda4c65a6
ghandylan <dylanlouis.tayag@yahoo.com>
2022-05-17 02:55:34 +0800

    changed

--- __init__.py
+++ __init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+my_database = SQLAlchemy()
+DATABASE_NAME = "database.my_database"
+DATABASE_USER = "ghandylan"
+DATABASE_PASSWORD = "hotdog123"
 
 def createApp():
     app = Flask(__name__, template_folder='templates')
     app.config['SECRET_KEY'] = 'mysecret'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{DATABASE_USER}:{DATABASE_PASSWORD}@localhost/{DATABASE_NAME}"
+    my_database.init_app(app)
 
     from views import views
     from auth import auth
---
+++ db_models.py
@@ -0,0 +1,31 @@
+from enum import unique
+from time import timezone
+from . import my_database
+from flask_login import UserMixin
+from sqlalchemy import func
+from sqlalchemy.dialects.mysql import *
+
+class Note(my_database.Model):
+    id = my_database.Column(my_database.Integer, primary_key=True)
+    data = my_database.Column(my_database.String(9999))
+    date = my_database.Column(my_database.DateTime(timezone=True), default=func.now())
+    user_id = my_database.Column(my_database.Integer, my_database.ForeignKey('user.id'))
+
+
+class User(UserMixin, my_database.Model):
+    id = my_database.Column(my_database.INT, primary_key=True)
+    name = my_database.Column(my_database.VARCHAR(255))
+    email = my_database.Column(my_database.VARCHAR(255), unique=True)
+    phone = my_database.Column(my_database.VARCHAR(255))
+    password = my_database.Column(my_database.VARCHAR(255))
+
+
+    def __init__(self, name, email, phone, password):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.password = password
+
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}', '{self.phone}', '{self.password}')"
